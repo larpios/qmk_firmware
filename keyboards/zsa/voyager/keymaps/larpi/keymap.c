b83abf3fde3b747fc9b6321876a77eafaf9fd43d
larpios <33772093+larpios@users.noreply.github.com>
2026-05-28 11:18:22 +0900

    feat: add brightness keys

--- keyboards/zsa/voyager/keymaps/larpi/keymap.c
+++ keyboards/zsa/voyager/keymaps/larpi/keymap.c
@@ -62,41 +62,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                        KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT
 ),
 
-//    ┌─────┬────┬────┬────┬────┬───────┐               ┌────┬────┬────┬────┬─────┬─────┐
-//    │ esc │ f1 │ f2 │ f3 │ f4 │  f5   │               │ f6 │ f7 │ f8 │ f9 │ f10 │ f11 │
-//    ├─────┼────┼────┼────┼────┼───────┤               ├────┼────┼────┼────┼─────┼─────┤
-//    │  `  │ <  │ >  │ (  │ )  │   *   │               │ \  │ 7  │ 8  │ 9  │  /  │ f12 │
-//    ├─────┼────┼────┼────┼────┼───────┤               ├────┼────┼────┼────┼─────┼─────┤
-//    │  ~  │ [  │ ]  │ {  │ }  │   =   │               │ +  │ 6  │ 5  │ 4  │  *  │ del │
-//    ├─────┼────┼────┼────┼────┼───────┤               ├────┼────┼────┼────┼─────┼─────┤
-//    │  &  │ ?  │ .  │ ,  │ !  │   $   │               │ -  │ 1  │ 2  │ 3  │  =  │ ins │
-//    └─────┴────┴────┴────┴────┼───────┼───────┬───────┼────┼────┴────┴────┴─────┴─────┘
+//    ┌─────┬────┬────┬────┬────┬───────┐               ┌────┬────┬────┬────┬───────┬─────┐
+//    │ esc │ f1 │ f2 │ f3 │ f4 │  f5   │               │ f6 │ f7 │ f8 │ f9 │  f10  │ f11 │
+//    ├─────┼────┼────┼────┼────┼───────┤               ├────┼────┼────┼────┼───────┼─────┤
+//    │  `  │ <  │ >  │ (  │ )  │   *   │               │ \  │ 7  │ 8  │ 9  │   /   │ f12 │
+//    ├─────┼────┼────┼────┼────┼───────┤               ├────┼────┼────┼────┼───────┼─────┤
+//    │  ~  │ [  │ ]  │ {  │ }  │   =   │               │ +  │ 6  │ 5  │ 4  │ print │ del │
+//    ├─────┼────┼────┼────┼────┼───────┤               ├────┼────┼────┼────┼───────┼─────┤
+//    │  &  │ ?  │ .  │ ,  │ !  │   $   │               │ -  │ 1  │ 2  │ 3  │ paus  │ ins │
+//    └─────┴────┴────┴────┴────┼───────┼───────┬───────┼────┼────┴────┴────┴───────┴─────┘
 //                              │ trans │ trans │ trans │ 0  │
 //                              └───────┴───────┴───────┴────┘
 [LAYER_SYMBOL] = LAYOUT_voyager(
-  KC_ESCAPE , KC_F1   , KC_F2   , KC_F3    , KC_F4   , KC_F5          ,                                   KC_F6    , KC_F7 , KC_F8 , KC_F9 , KC_F10   , KC_F11   ,
-  KC_GRAVE  , KC_LABK , KC_RABK , KC_LPRN  , KC_RPRN , KC_ASTR        ,                                   KC_BSLS  , KC_7  , KC_8  , KC_9  , KC_SLASH , KC_F12   ,
-  KC_TILD   , KC_LBRC , KC_RBRC , KC_LCBR  , KC_RCBR , KC_EQUAL       ,                                   KC_PLUS  , KC_6  , KC_5  , KC_4  , KC_ASTR  , KC_DELETE,
-  KC_AMPR   , KC_QUES , KC_DOT  , KC_COMMA , KC_EXLM , KC_DLR         ,                                   KC_MINUS , KC_1  , KC_2  , KC_3  , KC_EQUAL , KC_INSERT,
+  KC_ESCAPE , KC_F1   , KC_F2   , KC_F3    , KC_F4   , KC_F5          ,                                   KC_F6    , KC_F7 , KC_F8 , KC_F9 , KC_F10          , KC_F11   ,
+  KC_GRAVE  , KC_LABK , KC_RABK , KC_LPRN  , KC_RPRN , KC_ASTR        ,                                   KC_BSLS  , KC_7  , KC_8  , KC_9  , KC_SLASH        , KC_F12   ,
+  KC_TILD   , KC_LBRC , KC_RBRC , KC_LCBR  , KC_RCBR , KC_EQUAL       ,                                   KC_PLUS  , KC_6  , KC_5  , KC_4  , KC_PRINT_SCREEN , KC_DELETE,
+  KC_AMPR   , KC_QUES , KC_DOT  , KC_COMMA , KC_EXLM , KC_DLR         ,                                   KC_MINUS , KC_1  , KC_2  , KC_3  , KC_PAUSE        , KC_INSERT,
                                                        KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_0
 ),
 
 //    ┌─────────┬────────────────────┬──────────────────┬───────────────┬────────────────┬─────────────────┐               ┌─────────────────┬─────────┬───────┬───────┬───────┬─────────┐
 //    │ RGB_TOG │ TOGGLE_LAYER_COLOR │ RGB_MODE_FORWARD │    RGB_SLD    │    RGB_VAD     │     RGB_VAI     │               │      trans      │  trans  │ trans │ trans │ trans │ QK_BOOT │
 //    ├─────────┼────────────────────┼──────────────────┼───────────────┼────────────────┼─────────────────┤               ├─────────────────┼─────────┼───────┼───────┼───────┼─────────┤
-//    │  mprv   │       trans        │       vold       │     volu      │      mute      │      trans      │               │ LCTL(LSFT(tab)) │  home   │  up   │  end  │ trans │  trans  │
+//    │ bridown │       briup        │       vold       │     volu      │      mute      │      trans      │               │ LCTL(LSFT(tab)) │  home   │  up   │  end  │ trans │  trans  │
 //    ├─────────┼────────────────────┼──────────────────┼───────────────┼────────────────┼─────────────────┤               ├─────────────────┼─────────┼───────┼───────┼───────┼─────────┤
-//    │  trans  │        LGUI        │       LALT       │     LCTRL     │     LSHIFT     │      trans      │               │    LCTL(tab)    │  left   │ down  │ rght  │ RCTRL │  trans  │
+//    │  mnxt   │        LGUI        │       LALT       │     LCTRL     │     LSHIFT     │      trans      │               │    LCTL(tab)    │  left   │ down  │ rght  │ RCTRL │  trans  │
 //    ├─────────┼────────────────────┼──────────────────┼───────────────┼────────────────┼─────────────────┤               ├─────────────────┼─────────┼───────┼───────┼───────┼─────────┤
-//    │  trans  │       LCTRL        │      LSHIFT      │ HSV_0_255_255 │ HSV_74_255_255 │ HSV_169_255_255 │               │      trans      │ pAGE_UP │ pgdn  │ trans │ trans │  trans  │
+//    │  mprv   │       LCTRL        │      LSHIFT      │ HSV_0_255_255 │ HSV_74_255_255 │ HSV_169_255_255 │               │      trans      │ pAGE_UP │ pgdn  │ trans │ trans │  trans  │
 //    └─────────┴────────────────────┴──────────────────┴───────────────┴────────────────┼─────────────────┼───────┬───────┼─────────────────┼─────────┴───────┴───────┴───────┴─────────┘
 //                                                                                       │      trans      │ trans │ trans │      trans      │
 //                                                                                       └─────────────────┴───────┴───────┴─────────────────┘
 [LAYER_NAV] = LAYOUT_voyager(
   RGB_TOG             , TOGGLE_LAYER_COLOR , RGB_MODE_FORWARD  , RGB_SLD         , RGB_VAD        , RGB_VAI         ,                                   KC_TRANSPARENT     , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , QK_BOOT       ,
-  KC_MEDIA_PREV_TRACK , KC_TRANSPARENT     , KC_AUDIO_VOL_DOWN , KC_AUDIO_VOL_UP , KC_AUDIO_MUTE  , KC_TRANSPARENT  ,                                   LCTL(LSFT(KC_TAB)) , KC_HOME        , KC_UP          , KC_END         , KC_TRANSPARENT , KC_TRANSPARENT,
-  KC_TRANSPARENT      , KC_LEFT_GUI        , KC_LEFT_ALT       , KC_LEFT_CTRL    , KC_LEFT_SHIFT  , KC_TRANSPARENT  ,                                   LCTL(KC_TAB)       , KC_LEFT        , KC_DOWN        , KC_RIGHT       , KC_RIGHT_CTRL  , KC_TRANSPARENT,
-  KC_TRANSPARENT      , KC_LEFT_CTRL       , KC_LEFT_SHIFT     , HSV_0_255_255   , HSV_74_255_255 , HSV_169_255_255 ,                                   KC_TRANSPARENT     , KC_PAGE_UP     , KC_PGDN        , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT,
+  KC_BRIGHTNESS_DOWN  , KC_BRIGHTNESS_UP   , KC_AUDIO_VOL_DOWN , KC_AUDIO_VOL_UP , KC_AUDIO_MUTE  , KC_TRANSPARENT  ,                                   LCTL(LSFT(KC_TAB)) , KC_HOME        , KC_UP          , KC_END         , KC_TRANSPARENT , KC_TRANSPARENT,
+  KC_MEDIA_NEXT_TRACK , KC_LEFT_GUI        , KC_LEFT_ALT       , KC_LEFT_CTRL    , KC_LEFT_SHIFT  , KC_TRANSPARENT  ,                                   LCTL(KC_TAB)       , KC_LEFT        , KC_DOWN        , KC_RIGHT       , KC_RIGHT_CTRL  , KC_TRANSPARENT,
+  KC_MEDIA_PREV_TRACK , KC_LEFT_CTRL       , KC_LEFT_SHIFT     , HSV_0_255_255   , HSV_74_255_255 , HSV_169_255_255 ,                                   KC_TRANSPARENT     , KC_PAGE_UP     , KC_PGDN        , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT,
                                                                                                     KC_TRANSPARENT  , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT
 ),
 
